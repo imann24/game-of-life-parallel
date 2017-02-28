4d154e407936addca0d3043be154ba2403b921e7
Isaiah Mann <contact@isaiahmann.com>
2017-02-28 14:33:16 -0500

    Error checking for integer input

--- src/GameOfLifeApplication.java
+++ src/GameOfLifeApplication.java
@@ -11,10 +11,21 @@ public class GameOfLifeApplication {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		// Prompt to user
-		System.out.print("How many generations should the program run?  ");
+		// Prompt to user to enter a number of generations
+		promptUserForNumber(false);
+		String numGenStr = "";
+		while(keyboard.hasNextLine()) {
+			numGenStr = keyboard.nextLine();
+			// Error checking in case user has not entered an int
+			if(isInt(numGenStr)) {
+				break;
+			} else {
+				// Prompt to user to enter a valid number
+				promptUserForNumber(true);
+			}
+		}
 		// Read in number of generations to run program for from keyboard
-		int numGen = keyboard.nextInt();
+		int numGen = Integer.parseInt(numGenStr);
 		
 		BlockingQueue<Integer> firstThreadQueue = new ArrayBlockingQueue<Integer>(1);
 		BlockingQueue<Integer> secondThreadQueue = new ArrayBlockingQueue<Integer>(1);
@@ -43,13 +54,34 @@ public class GameOfLifeApplication {
 		while(firstThread.isAlive() || secondThread.isAlive()) {
 			clearScreen();
 			print(GameOfLifeData.currDish);
-			// Wait a millisecond before trying to print again
 			try {
+				// guarantees that one generation is not printed twice
+				// gives time for next generation to be computed
 				Thread.sleep(GameOfLifeData.CONSOLE_DELAY);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	static void promptUserForNumber (boolean isRepeat) {
+		// Prompt to user
+		System.out.println();
+		if(isRepeat) {
+			System.out.print("Please enter a valid number for number of generations  ");
+		} else {
+			System.out.print("How many generations should the program run?  ");
+		}
+	}
+	
+	static boolean isInt(String str)   {  
+	  try   {  
+	    Integer.parseInt(str);	  
+	    return true;  
+	  }  
+	  catch(NumberFormatException nfe) {  
+	    return false;  
+	  }    
 	}
 	
 	static void print(String[] dish) {

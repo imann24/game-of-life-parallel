6fedba0cc3a3da47fa26989681b166273f906cc1
Isaiah Mann <contact@isaiahmann.com>
2017-02-28 14:11:59 -0500

    Printing dynamically (async:

--- src/GameOfLifeApplication.java
+++ src/GameOfLifeApplication.java
@@ -16,23 +16,8 @@ public class GameOfLifeApplication {
 		// Read in number of generations to run program for from keyboard
 		int numGen = keyboard.nextInt();
 		
-		// Determines whether program prints every generation
-		boolean printEveryGen = false;
-		String response = keyboard.nextLine();
-		while(!(response.equals("y") || response.equals("n"))) {
-			System.out.print("Print every generation (y/n)?  ");
-			response = keyboard.nextLine();
-		}
-		if(response.equals("y")) {
-			printEveryGen = true;
-		}		
-		
 		BlockingQueue<Integer> firstThreadQueue = new ArrayBlockingQueue<Integer>(1);
 		BlockingQueue<Integer> secondThreadQueue = new ArrayBlockingQueue<Integer>(1);
-		
-		// Controls when the manager prints (if printing every generation)
-		BlockingQueue<Integer> firstThreadManagerQueue = new ArrayBlockingQueue<Integer>(1);
-		BlockingQueue<Integer> secondThreadManagerQueue = new ArrayBlockingQueue<Integer>(1);
 		
 		// Breaking up the dish for threads
 		int numRowsInDish = GameOfLifeData.newDish.length;
@@ -41,49 +26,44 @@ public class GameOfLifeApplication {
 		GameOfLifeThread firstThread = new GameOfLifeThread(
 				secondThreadQueue, 
 				firstThreadQueue,
-				firstThreadManagerQueue,
 				0,
 				middleRow - 1,
-				numGen,
-				printEveryGen);
+				numGen);
 		
 		GameOfLifeThread secondThread = new GameOfLifeThread(
 				firstThreadQueue, 
 				secondThreadQueue,
-				secondThreadManagerQueue,
 				middleRow,
 				numRowsInDish - 1,
-				numGen,
-				printEveryGen);
+				numGen);
 		
 		// Use "start" instead of "run" because "run" blocks the main thread
 		firstThread.start();
 		secondThread.start();
-		if(printEveryGen) {  // CASE A: Synchronized printing
-			for(int i = 0; i < numGen; i++) {
-				try {
-					firstThreadManagerQueue.take();
-					secondThreadManagerQueue.take();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				print(GameOfLifeData.newDish);
-			}
-		} else { // CASE B: Printing last generation
+		while(firstThread.isAlive() || secondThread.isAlive()) {
+			clearScreen();
+			print(GameOfLifeData.currDish);
+			// Wait a millisecond before trying to print again
 			try {
-				firstThread.join();
-				secondThread.join();
+				Thread.sleep(GameOfLifeData.CONSOLE_DELAY);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			print(GameOfLifeData.newDish);
 		}
 	}
 	
-	public static void print(String[] dish) {
+	static void print(String[] dish) {
 		for(String s: dish) {
 		    System.out.println(s);
 	    }
 	}
+
+    static void clearScreen () {
+    		final String ANSI_CLS = "\u001b[2J";
+        final String ANSI_HOME = "\u001b[H";
+        System.out.print(ANSI_CLS + ANSI_HOME);
+        System.out.print(ANSI_HOME);
+        System.out.flush();
+    }
 
 }
